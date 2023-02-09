5bf3fa411c731118e34de8f8c655eb64bb8ca079
TammamWafai <tammam.wafai@gmail.com>
2023-02-08 18:15:17 -0600

    answers for lesson 9

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,6 +2,8 @@ const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
+    console.log(err.message);
+
     return res.status(err.statusCode).json({ msg: err.message })
   }
   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
